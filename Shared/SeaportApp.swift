101111b9e05358f983128988913927cfd2b9556e
Patrick Smith <pgwsmith@gmail.com>
2021-02-22 15:20:48 +1100

    Initial Commit

---
+++ Shared/SeaportApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeaportApp.swift
+//  Shared
+//
+//  Created by Patrick Smith on 22/2/21.
+//
+
+import SwiftUI
+
+@main
+struct SeaportApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
